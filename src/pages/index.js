4e7705ffcd471fa4b4a9a6ff4ff03a37987cc4ff
dhairyasharma504 <dhairyasharma2311@gmail.com>
2023-03-10 19:16:20 +0530

    ff

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import About from '../components/About';
 import Blog from '../components/Blog';
 
 import Layout from '../components/global/Layout';
-import InstagramGallery from '../components/InstagramGallery';
+
 import Price from '../components/Price';
 import Products from '../components/Products';
 import Rating from '../components/Rating';
