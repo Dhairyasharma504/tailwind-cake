5555806073e45bb527d837d098fcb4d9a0e3a57f
dhairyasharma504 <dhairyasharma2311@gmail.com>
2023-03-10 15:28:04 +0530

    fix

--- src/components/elements/BlogCard.js
+++ src/components/elements/BlogCard.js
@@ -13,9 +13,7 @@ function BlogCard() {
             className=" h-full"
           />
           <div className="bg-white absolute top-0 left-0">
-            <p className="text-base leading-4 py-3 px-5 text-gray-800 dark:text-white dark:bg-gray-900">
-              News
-            </p>
+            <p className="text-base leading-4 py-3 px-5 text-gray-800 ">News</p>
           </div>
         </div>
         <div className="p-6 py-10 space-y-8 border border-gray-500 last:border-x-0">
