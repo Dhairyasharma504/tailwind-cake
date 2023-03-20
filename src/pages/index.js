cef02da2b3562c4f01f13475789216457c756422
dhairyasharma504 <dhairyasharma2311@gmail.com>
2023-03-20 15:42:04 +0530

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import Blog from '../components/Blog';
 
-import Layout from '../components/global/Layout';
-
 import Price from '../components/Price';
 import Products from '../components/Products';
 import Rating from '../components/Rating';
+
+const Layout = dynamic(() => import('../components/global/Layout'), {
+  ssr: false,
+});
 
 const About = dynamic(() => import('../components/About'), {
   ssr: false,
